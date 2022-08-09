b2b4ff74bd041d935d4c4fd34b94ef8479b6f207
Артём Токарев <artemabushahmin2000@gmail.com>
2022-08-09 16:11:03 +0500

    dz

---
+++ yatube/ice_cream/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+# Главная страница
+def index(request):    
+    template = 'ice_cream/index.html'
+    return render(request, template) 
+
+
+# Страница со списком мороженого
+def ice_cream_list(request):
+    return HttpResponse('Список мороженого')
+
+
+# Страница с информацией об одном сорте мороженого;
+# view-функция принимает параметр pk из path()
+def ice_cream_detail(request, pk):
+    return HttpResponse(f'Мороженое номер {pk}') 
